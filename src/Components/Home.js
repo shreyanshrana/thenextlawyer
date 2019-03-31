7061d7656ae7a2d4eaea4268d997f48fe8a0e178
pratyush2704 <pratyush8smart@gmail.com>
2019-03-31 23:09:28 +0530

    why_us_final

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import BottomNav from "./Home/BottomNav";
 import Landing from "./Home/Landing";
+import "./Home/Navbar.scss";
+
 export default class Home extends Component {
   render() {
     return (
